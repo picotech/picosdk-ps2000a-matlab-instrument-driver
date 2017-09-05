7b8a54b30b65cd0d0cde3f8b5888b35b453e798c
hsmistry <hsmistry@users.noreply.github.com>
2017-09-05 18:54:35 +0100

    Update script formatting

--- examples/PS2000A_ID_Streaming_Example.m
+++ examples/PS2000A_ID_Streaming_Example.m
@@ -493,7 +493,7 @@ if (totalSamples < maxSamples)
  
 end
 
-% Retrieve data for the Channels.
+% Retrieve data for the channels.
 channelAFinal = pBufferChAFinal.Value();
 channelBFinal = pBufferChBFinal.Value();
 
@@ -524,7 +524,7 @@ ylabel(finalFigureAxes, 'Voltage (mV)');
 maxYRange = max(channelARangeMv, channelBRangeMv);
 ylim(finalFigureAxes,[(-1 * maxYRange) maxYRange]);
 
-% Calculated values for time axis, then plot.
+% Calculate values for time axis, then plot.
 timeAxis = (double(actualSampleInterval) * double(downSampleRatio)) * (0:length(channelAFinal) - 1);
 plot(timeAxis, channelAFinal, timeAxis, channelBFinal);
 
